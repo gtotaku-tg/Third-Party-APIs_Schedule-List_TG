74dda6d5c94b2a9bea8a75395f7365836737ed92
gtotaku-tg <124806960+gtotaku-tg@users.noreply.github.com>
2023-07-13 16:09:22 -0400

    delete unused files js and css file up to dated readme file updated

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,18 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+// $(document).ready(function () {});
+
+// Set variables to get the current date and time and display it in the header
+var currentDayEl = $("#currentDay");
+function displayDate() {
+  var currentDate = dayjs().format("dddd, MMMM D, YYYY， HH:mm:ss a");
+  currentDayEl.text(currentDate);
+}
+// displayDate function is called
+displayDate();
+// Set interval to update the current date and time every second
+setInterval(displayDate, 1000);
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -21,4 +33,30 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  var currentHour = dayjs().hour();
+  console.log("currentHour", currentHour);
+  $(".time-block").each(function () {
+    var hour = parseInt($(this).attr("id").split("-")[1]);
+    console.log("hour", hour);
+    if (hour < currentHour) {
+      $(this).addClass("past");
+    } else if (hour == currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
+  $("#clearBtn").on("click", function () {
+    localStorage.clear();
+    location.reload();
+  });
+
+  $(".saveBtn").on("click", function () {
+    var time = $(this).parent().attr("id");
+    var text = $(this).siblings(".description").val();
+    localStorage.setItem(time, text);
+  });
+  for (var i = 9; i < 18; i++) {
+    $("#hour-" + i + " .description").val(localStorage.getItem("hour-" + i));
+  }
 });
